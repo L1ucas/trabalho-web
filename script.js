271cad8c7b86220bfb8a1a41e1edbeb5399a80da
L1ucas <1lucascode@gmail.com>
2022-12-08 19:47:01 -0300

    Api backup and few changes to the svgs and the script.js

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
 let input_entrada = document.getElementById("entrada")
 let input_saida = document.getElementById("saida")
 
+
 let valor_entrada = 0
 let valor_saida = 1
 
-const url = 'https://economia.awesomeapi.com.br/last/USD-BRL,USD-EUR,EUR-BRL,EUR-USD,BRL-USD,BRL-EUR'
-
+// const url = 'https://economia.awesomeapi.com.br/last/USD-BRL,USD-EUR,EUR-BRL,EUR-USD,BRL-USD,BRL-EUR'
+const url = './api.json'
 
 // D, R, E
 
 let conversoes = [
   //   D          R             E
-  [1, 5, 2], [1/5, 1, 1/2], [1/2, 2, 1]
+  [1, 5.25, 0.95], [0.19, 1, 0.18], [1.06, 5.54, 1]
 ]
 
 
@@ -25,7 +26,7 @@ async function pegarMoedas(){
   conversoes[0][1] = Number(moedas.USDBRL.high)
   conversoes[0][2] = Number(moedas.USDEUR.high)
   conversoes[1][0] = Number(moedas.BRLUSD.high)
-  conversoes[1][0] = 1
+  conversoes[1][1] = 1
   conversoes[1][2] = Number(moedas.BRLEUR.high)
   conversoes[2][0] = Number(moedas.EURUSD.high)
   conversoes[2][1] = Number(moedas.EURBRL.high)
@@ -60,9 +61,14 @@ function imprimirValor(e) {
 }
 
 function converterValores(){
-  
-  input_saida.value = (input_entrada.value * conversoes[valor_entrada][valor_saida]).toFixed(2)
-  console.log(conversoes[valor_entrada][valor_saida])
+  let regex = /[0-9.,]+$/
+  if (!regex.test(input_entrada.value) && input_entrada.value != ""){
+    input_saida.value = "Valor inválido"
+  }else{
+    let value_user = Number(input_entrada.value.replace(",", "."))
+    input_saida.value = (value_user * conversoes[valor_entrada][valor_saida]).toFixed(2)
+  }
+    console.log(conversoes[valor_entrada][valor_saida])
 }
 
 // $("#").blur(function(e) {
